f22d270af437aa1cd3ab074a749c8801633fb5ee
nknglee <50165480+nknglee@users.noreply.github.com>
2025-06-06 21:28:56 +0100

    Update aDNA_part3.sh
    Added QC for final bam files

--- aDNA_part3.sh
+++ aDNA_part3.sh
@@ -52,6 +52,16 @@ echo -e ${UPDATED_SAMPLE}'\t'${HOM}/mapped/${UPDATED_SAMPLE}.RG.mapped.sort.rmdu
 ${BIN}/AMBER/AMBER --bamfiles ${HOM}/aDNA_authentication/${UPDATED_SAMPLE}.tsv --output ${HOM}/aDNA_authentication/${UPDATED_SAMPLE} --errorbars --counts
 check_command "AMBER"
 
+# QC
+## Mapped reads
+echo "SAMPLE=${HOM}/mapped/${UPDATED_SAMPLE}.RG.mapped.sort.rmdup.bam" >> ${HOM}/QC/quality_check_${UPDATED_SAMPLE}.txt
+echo -e "\nMapped reads (samtools flagstat):" >> ${HOM}/QC/quality_check_${UPDATED_SAMPLE}.txt
+samtools flagstat ${HOM}/mapped/${UPDATED_SAMPLE}.RG.mapped.sort.rmdup.bam >> ${HOM}/QC/quality_check_${UPDATED_SAMPLE}.txt
+
+## Read stats
+echo -e "\nRead stats (samtools stats):" >> ${HOM}/QC/quality_check_${UPDATED_SAMPLE}.txt
+samtools stats ${HOM}/mapped/${UPDATED_SAMPLE}.RG.mapped.sort.rmdup.bam | grep ^SN | cut -f 2- >> ${HOM}/QC/quality_check_${UPDATED_SAMPLE}.txt
+
 # Haplotype calling - GATK
 gatk --java-options -Xmx32G HaplotypeCaller -R ${REF} -I ${HOM}/mapped/${UPDATED_SAMPLE}.RG.mapped.sort.rmdup.bam -O ${HOM}/gvcf/${UPDATED_SAMPLE}.RG.mapped.sort.rmdup.g.vcf.gz -ERC GVCF
 check_command "GATK"
