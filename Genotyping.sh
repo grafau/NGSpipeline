e750913ac07f705aa05123506a940ef3f5448261
nknglee <50165480+nknglee@users.noreply.github.com>
2025-07-21 12:05:57 +0100

    Update Genotyping.sh
    Changes in g.vcf file names, and minor things suitable for Crop Diversity HPC environment.
    New directory structure (separate directory for genotyping rather than read processing work directory)

--- Genotyping.sh
+++ Genotyping.sh
@@ -1,38 +1,38 @@
 #!/bin/bash
 #SBATCH --verbose
-#SBATCH --mem=20G
-#SBATCH -c 12
-#SBATCH -p all
+#SBATCH --mem=32G
+#SBATCH -c 16
+#SBATCH -p himem
 #SBATCH -J Genotyping
-#SBATCH -t 0-72:00:00
-#SBATCH -o /path/to/log/Genotyping_%j.log
-#SBATCH -e /path/to/log/Genotyping_%j.err
+#SBATCH -t 0-330:00:00
+#SBATCH -o log/Genotyping_%j.log
+#SBATCH -e log/Genotyping_%j.err
 
 #module load & libraries
-module purge
 eval "$(conda shell.bash hook)"
 conda activate ngs
 
-#Submit command: sbatch Genotyping.sh
+#Submit command: sbatch ./Genotyping.sh
 
 #Print the task ID
 cd "$SLURM_SUBMIT_DIR"
 echo "My SLURM_JOB_ID: " $SLURM_JOB_ID
 
 #Variables
+DAT=/path/to/g.vcf
 REF=/path/to/ref
 HOM=/path/to/working/directory
 
 
 #Code
 #Sample list for genotyping
-ls ${HOM}/gvcf/*.RG.mapped.sort.rmdup.g.vcf.gz > ${HOM}/gvcf/gvcf.list
+ls ${DAT}/*.RG.mapped.sorted.rmdup.sorted.g.vcf.gz > ${HOM}/gvcf.list
 
 #Combine GVCFs
-gatk --java-options "-Xmx32G" CombineGVCFs -R ${REF}/IRGSP-1.0_genome.fasta --variant ${HOM}/gvcf/gvcf.list -O ${HOM}/gvcf/combined.g.vcf.gz
+gatk --java-options "-Xmx32G" CombineGVCFs -R ${REF}/IRGSP-1.0_genome.fasta --variant ${HOM}/gvcf.list -O ${HOM}/combined.g.vcf.gz
 
 # Validate Variants
-gatk --java-options "-Xmx32G" ValidateVariants -V ${HOM}/gvcf/combined.g.vcf.gz
+gatk --java-options "-Xmx32G" ValidateVariants -V ${HOM}/combined.g.vcf.gz
 
 # Genotype GVCFs
-gatk --java-options "-Xmx32G" GenotypeGVCFs -R ${REF}/IRGSP-1.0_genome.fasta -V ${HOM}/gvcf/combined.g.vcf.gz -O ${HOM}/vcf/combined.vcf.gz
+gatk --java-options "-Xmx32G" GenotypeGVCFs -R ${REF}/IRGSP-1.0_genome.fasta -V ${HOM}/combined.g.vcf.gz -O ${HOM}/combined.vcf.gz
